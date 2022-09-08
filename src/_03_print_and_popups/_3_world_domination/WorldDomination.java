fe25776d278bcbb0ca30c537f40d5c44ebeb1b80
league <league@imac-109.attlocal.net>
2022-09-07 17:00:35 -0700

    quiz game

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -6,9 +6,17 @@ import javax.swing.JOptionPane;
 public class WorldDomination {
 	
 	public static void main(String[] args) {
-		
+		@SuppressWarnings("unused")
+		String input = JOptionPane.showInputDialog("Do you know how to write code");
 		// 1. Ask the user if they know how to write code.
-
+	if(input.equals("yes")) {
+		JOptionPane.showMessageDialog(null,"You will rule the world");
+	
+	}
+	else {
+		JOptionPane.showMessageDialog(null,"Go sign up for classes at The league of Programers.....idiot");
+	}
+		
 		// 2. If they say "yes", tell them they will rule the world.
 
 		// 3. Otherwise, tell them to sign up for classes at The League.
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,16 +1,28 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+	int score=0;
+			
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
+	String input = JOptionPane.showInputDialog("waffles or pancakes");
 				// 2.  Ask the user a question 
-				
-				// 3.  Use an if statement to check if their answer is correct
-				
+		if(input.equals("both")){
+			score=score+1;
+			JOptionPane.showMessageDialog(null,score);
+			// 3.  Use an if statement to check if their answer is correct
+		}
+		 input = JOptionPane.showInputDialog("waffles or pancakes");
+		// 2.  Ask the user a question 
+if(input.equals("both")){
+	score=score+1;
+	JOptionPane.showMessageDialog(null,score);
+	// 3.  Use an if statement to check if their answer is correct
+}	
 				// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
